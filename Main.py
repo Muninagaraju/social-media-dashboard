cb4d59a07c6696e6f15efb4f71baf0302b2abec0
Muninagaraju <gudiboyinamuninagaraju@Gmail>
2026-05-12 14:11:52 +0530

    Fixed streamlit import and removed MySQL

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
+import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
 
 # -------------------------------
 # Connect to MySQL
@@ -12,7 +15,6 @@ def load_data():
     return df
 
 df = load_data()
-
 # -------------------------------
 # Load Full Dataset (Single Query)
 # -------------------------------
